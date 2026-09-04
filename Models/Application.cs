3caf0560945cc0fc02514c3cd3a0182bfe1228f2
SyazwaniIzzati <nursyazwaniizzati4@gmail.com>
2026-09-04 10:52:05 +0800

    add summary fill

--- Models/Application.cs
+++ Models/Application.cs
@@ -6,5 +6,6 @@ public class Application
     public Guid JobId { get; set; }
     public string CandidateName { get; set; } = string.Empty;
     public string CandidateEmail { get; set; } = string.Empty;
+    public string Summary {get; set; } = string.Empty;
     public DateTime SubmittedDateTime { get; set; }
 }
